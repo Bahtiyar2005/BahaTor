e8d855e8a5aceb21f242fd1fa2e91c906e186658
STUDENT10 <student10@Nur-Ordas-iMac-2.local>
2020-03-11 11:43:11 +0600

    2nd commit

--- project.py
+++ project.py
@@ -12,7 +12,40 @@ button5 = Button(window, text = "Standup")
 button6 = Button(window, text = "VideoStandup")
 image = Label(window, text = "Image")
 lol = Label(window, text = "")
+lol1 = Label(window, text = "")
+lol2 = Label(window, text = "")
+lol3 = Label(window, text = "<")
+lol4 = Label(window, text = ">")
 
+
+root = Tk()
+root.title("Tk dropdown example")
+
+# Add a grid
+button11 = Frame(root)
+button11.grid(column=0,row=0, sticky=(N,W,E,S) )
+button11.columnconfigure(0, weight = 1)
+button11.rowconfigure(0, weight = 1)
+button11.pack(pady = 100, padx = 100)
+
+
+# Create a Tkinter variable
+tkvar = StringVar(root)
+
+# Dictionary with options
+choices = { 'Black Humor','Animals','School','All','Standups','Video Standups'}
+tkvar.set('Black Humor') # set the default option
+
+popupMenu = OptionMenu(button11, tkvar, *choices)
+Label(button11, text="Categories").grid(row = 1, column = 1)
+popupMenu.grid(row = 2, column =1)
+
+# on change dropdown value
+def change_dropdown(*args):
+    print( tkvar.get() )
+
+# link function to change dropdown
+tkvar.trace('w', change_dropdown)
 
 
 button1.grid(row = 0, column = 0)
@@ -21,9 +54,15 @@ button3.grid(row = 2, column = 0)
 button4.grid(row = 3, column = 0)
 button5.grid(row = 4, column = 0)
 button6.grid(row = 5, column = 0)
-lol.grid(row = 5, column = 4)
+lol.grid(row = 5, column = 2, ipadx = 15)
+lol1.grid(row = 5, column = 3, ipadx = 15)
+lol2.grid(row = 5, column = 4, ipadx = 15)
+lol3.grid(row = 5, column = 5)
+lol4.grid(row = 5, column = 7)
 
-image.grid(row = 4, column = 6, columnspan = 4, rowspan = 4)
+
+
+image.grid(row = 4, column = 6)
 
 button1.grid(row = 0, column = 0, ipadx = 55)
 button2.grid(row = 1, column = 0 , ipadx = 71)
